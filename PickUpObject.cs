02c552caaa3d59e4779d59648fb6b0f68f8b4d47
Marco Bosquez <60762402+MarcoBosglez@users.noreply.github.com>
2020-02-06 20:01:23 -0600

    Create PickUpObject.cs

---
+++ PickUpObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player") {
+            Debug.Log ("Recojiste el paquete!");
+            Destroy (gameObject);
+        }
+    }
+}
